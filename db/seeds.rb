88d5359a1066563ef7e52f3b217ca1aa6eec66b0
Lucy Fox <lucyreneefox@gmail.com>
2017-01-03 15:41:03 -0700

    Add exercises csv to seed file and seed db with categories and exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,29 @@
+require 'csv'
+
 class Seed
   def self.start
     seed = Seed.new
-
+    seed.categories
+    seed.exercises
   end
 
-  def seed_categories
+  def categories
+    puts "creating categories!"
     Category.create!(name: "LISS")
     Category.create!(name: "HIIT")
     Category.create!(name: "Resistance-Arms")
     Category.create!(name: "Resistance-Legs")
     Category.create!(name: "Resistance-Abs")
     Category.create!(name: "Resistance-Full-Body")
+    puts "Done creating categories"
+  end
+
+  def exercises
+    puts "creating exercises"
+    CSV.foreach('db/exercises.csv', headers: true) do |row|
+      Exercise.create!(name: row["name"], max_weight: 0, category_id: row["category"].to_i)
+    end
+    puts "all exercises in db"
   end
 end
 
