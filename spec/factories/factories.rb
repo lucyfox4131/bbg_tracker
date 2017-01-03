cd4d9e949f0ddf3a388a61b9b38f9984b7c9885f
Lucy Fox <lucyreneefox@gmail.com>
2017-01-03 13:40:28 -0700

    Add test for name to be required when creating a new category as well as set up factory girl for category and verify its working

---
+++ spec/factories/factories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :category do
+    name ["LISS", "HIIT", "Resistance-Arm", "Resistance-Leg", "Resistance-Full-Body"].sample
+  end
+end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+
+  it "should have a name if the factory works" do
+    category = create(:category, name: "LISS")
+    expect(category.name).to eq("LISS")
+  end
 end
