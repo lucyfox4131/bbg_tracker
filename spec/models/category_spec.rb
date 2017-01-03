fe1bbd9ef7febfab13dd30d75adc97acde59479c
Lucy Fox <lucyreneefox@gmail.com>
2017-01-03 13:58:46 -0700

    Add shoulda matcher tests to verify relationships between reps exercises and categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { should validate_presence_of(:name) }
-
+  it { should have_many(:exercises) }
+  
   it "should have a name if the factory works" do
     category = create(:category, name: "LISS")
     expect(category.name).to eq("LISS")
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
   it { should validate_presence_of(:name) }
-
-    
+  it { should belong_to(:category) }
+  it { should have_many(:reps)}
 end
