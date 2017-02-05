537934e9aeb260cd4a0b912f51ae94f05b280e0f
Lucy Fox <lucyreneefox@gmail.com>
2017-02-05 13:09:45 -0700

    registered user click and set up exercise api route

--- app/assets/javascripts/allExercises.js
+++ app/assets/javascripts/allExercises.js
@@ -1,4 +1,7 @@
 function allExercsies(){
+  $("#exercise-btn").click(function(){
+    console.log("Exercise btn clicked!")
+  });
   // write this up
   // when all exercises button is clicked show a list of exercises
 };
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   resources :home, only: [:show]
 
   root to: "home#show"
+
+  namespace :api do
+    namespace :v1 do
+      resources :exercises, only: [:index, :show]
+    end
+  end
 end
